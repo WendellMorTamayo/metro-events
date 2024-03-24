31b78235f58f639281c367f66ecf032056e8a313
windss <tamayowendell05@gmail.com>
2024-03-25 01:47:47 +0800

    chore: implement stripe

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,18 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import Collection from "@/components/shared/Collection";
+import { getAllEvents } from "@/lib/mongodb/actions/event.actions";
 
-export default function Home() {
+export default async function Home() {
+  const events = await getAllEvents({
+    query: {},
+    category: "",
+    page: 1,
+    limit: 6,
+  });
+
+  console.log(events);
   return (
     <>
       <section
@@ -44,6 +54,15 @@ export default function Home() {
         <div className={"flex w-full flex-col gap-5 md:flex-row"}>
           Search CategoryFilter
         </div>
+        <Collection
+          data={events?.data}
+          emptyTitle="No events found"
+          emptyStateSubtext="Coome back later"
+          collectionType={"All_Events"}
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
       </section>
     </>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,7 @@ const config = withUt({
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx,mdx}",
   ],
   theme: {
     container: {
